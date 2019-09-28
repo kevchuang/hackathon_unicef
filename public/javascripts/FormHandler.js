8aca671eb137969270dccebad68ef9669569c3bc
riiswa <wradji64@gmail.com>
2019-09-29 00:47:52 +0200

    Add FromHandler.js

---
+++ public/javascripts/FormHandler.js
@@ -0,0 +1,40 @@
+var Constants = require('./../utils/Constants.js');
+var FormValidator = require('./../javascripts/FormValidator.js');
+
+const FormHandler = {
+    parseForm(smsObject) {
+    var parsingSMS = {};
+    var i = 0;
+    var splitSMS = smsObject.message.split('\n');
+    splitSMS.forEach(field => {
+        var splitField = field.split(")");
+        if (splitField.length === 2) {
+            parsingSMS[Constants.CERTIFICATE_ARRAY[i]] = splitField[1].trim();
+            i++;
+        }
+    });
+    parsingSMS.phoneNumber = smsObject.phoneNumber;
+    return parsingSMS;
+    },
+
+    validateForm(smsObject) {
+        var parsingSMS = FormHandler.parseForm(smsObject);
+
+        var errorMessage = FormValidator.checkAllData(parsingSMS);
+
+        if (errorMessage === "") {
+            return {'message': parsingSMS, 'code': Constants.HTTPCode.SUCCESS};
+        } else {
+            return {
+                'message': "Error(s) were spotted : " + errorMessage.substring(0, errorMessage.length - 2) ,
+                'code': Constants.HTTPCode.BAD_REQUEST
+            };
+            // "are" can be replace by "is" in certain case
+        }
+
+    }
+
+};
+
+module.exports = FormHandler;
+
--- public/utils/HTTPCode.js
+++
@@ -1,6 +0,0 @@
-const HTTPCode = {
-    SUCCESS: 200,
-    BAD_REQUEST: 400
-};
-
-module.exports = HTTPCode;
