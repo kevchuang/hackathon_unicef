372c58aea6a14d4a849d021d3214511b67ca0a51
GerminalCorreia <GerminalCorreia@users.noreply.github.com>
2019-09-29 12:33:29 +0200

    get certificate

--- public/utils/Constants.js
+++ public/utils/Constants.js
@@ -5,7 +5,8 @@ const Constants = {
         BAD_REQUEST: 400
     },
 
- CERTIFICATE_ARRAY: ["firstname", "middlename", "lastname", "gender", "birthPlace", "birthdate", "parent1", "parent2", "dateRecognitionParent1", "dateRecognitionParent2"]
+ CERTIFICATE_ARRAY: ["firstname", "middlename", "lastname", "gender", "birthPlace", "birthdate", "parent1", "parent2", "dateRecognitionParent1", "dateRecognitionParent2"],
+ RETRIEVE_CERTIFICATE_ARRAY: ["firstname", "middlename", "lastname", "gender", "birthPlace", "birthdate"]
 
 };
 
